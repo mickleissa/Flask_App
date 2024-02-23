b51c924d24387bab69e38a925ce88aafc86c6259
Mickleissa <51688991+mickleissa@users.noreply.github.com>
2024-02-23 00:32:48 -0500

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-987654321"
     key    = "devops-project-1/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-west-1"
   }
 }
