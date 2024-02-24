453e504f093bb0e089b8f45a3e5f5fd0c13bd461
Mickleissa <51688991+mickleissa@users.noreply.github.com>
2024-02-24 01:28:03 -0500

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -6,7 +6,7 @@ yes | sudo apt install python3 python3-pip
 git clone https://github.com/mickleissa/Flask_App_python_mysql_db.git
 sleep 20
 # shellcheck disable=SC2164
-cd python-mysql-db-proj-1
+cd Flask_App_python_mysql_db
 sudo pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 sudo setsid python3 -u app.py &
