1fb4d449c2a6fc916a3719d0c9fc94d01464ecda
Mickleissa <51688991+mickleissa@users.noreply.github.com>
2024-02-24 01:28:26 -0500

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -7,7 +7,7 @@ git clone https://github.com/mickleissa/Flask_App_python_mysql_db.git
 sleep 20
 # shellcheck disable=SC2164
 cd Flask_App_python_mysql_db
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
-sudo setsid python3 -u app.py &
+setsid python3 -u app.py &
 sleep 30
