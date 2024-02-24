c3089049cf570f83e74892259ad661fb2ce1a7b7
Mickleissa <51688991+mickleissa@users.noreply.github.com>
2024-02-24 00:31:32 -0500

    Update main.tf

--- infra/hosted-zone/main.tf
+++ infra/hosted-zone/main.tf
@@ -4,7 +4,7 @@ variable "aws_lb_zone_id" {}
 
 data "aws_route53_zone" "dev_proj_1_jhooq_org" {
   name         = "mickleissa.com"
-  name         = var.domain_name
+ # name         = var.domain_name
   private_zone = false
 }
 
